189e1c81757959f7cc6ba6582c426e95ce922e6b
Liutauras Razma <leq382121@gmail.com>
2019-09-19 09:50:41 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,16 +6,10 @@ echo "check 'mp3' or 'downloaded' folders when terminal closes."
 pause
 REM echo "----"
 REM echo "Before we start, you need to get a link to spotify playlist - Press ALT + SPACE + E + P"
-REM echo "We will use your facebook login to access spotify. "
-REM echo "We don't store it anywhere, no worries. "
 REM pause
 REM SET /P spotifylink=Paste your spotify link:
 REM IF "%spotifylink%"=="" GOTO Error
-REM SET /P uname=Enter your facebook login:
-REM IF "%uname%"=="" GOTO Error
-REM SET /P upass=Enter your facebook pass:
-REM IF "%upass%"=="" GOTO Error
-REM node spotube.js %uname% %upass% %spotifylink%
+REM node spotube.js %spotifylink%
 node spotube.js
 node c2mp3.js
 exit
